59a19904a59be40bc05b34f659944826ab625739
Kelvim Rodrigues <kelvimrodrigues1@gmail.com>
2026-08-04 16:51:07 -0300

    add endpoints update, delete, post

--- Mango/Mango.Services.CouponAPI/Controllers/CouponController.cs
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -1,4 +1,5 @@
-﻿using Mango.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
@@ -11,7 +12,7 @@ public class CouponController : ControllerBase
 {
     private readonly AppDbContext _dbcontext;
     private readonly ResponseDto _response;
-
+   
     public CouponController(AppDbContext dbcontext)
     {
         _dbcontext = dbcontext;
@@ -23,7 +24,7 @@ public class CouponController : ControllerBase
     {
         try
         {
-            IEnumerable<Coupon> objList = _dbcontext.Coupons.ToList();
+            IEnumerable<Coupon> objList = _dbcontext.Coupons.ToList();            
             _response.Result = objList;
             
         }
@@ -43,7 +44,114 @@ public class CouponController : ControllerBase
         try
         {
             Coupon objList = _dbcontext.Coupons.First(c => c.CouponId == id);
-            _response.Result = objList;
+            CouponDto couponDto = new CouponDto
+            {
+                CouponId = objList.CouponId,
+                CouponCode = objList.CouponCode,
+                DiscountAmount = objList.DiscountAmount,
+                MinAmount = objList.MinAmount
+            };
+
+            _response.Result = couponDto;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+
+        return _response;
+    }
+
+    [HttpGet]
+    [Route("GetByCode/{code}")]
+    public ResponseDto GetByCode(string code)
+    {
+        try
+        {
+            Coupon objList = _dbcontext.Coupons.First(c => c.CouponCode.ToLower() == code.ToLower());
+            CouponDto couponDto = new CouponDto
+            {
+                CouponId = objList.CouponId,
+                CouponCode = objList.CouponCode,
+                DiscountAmount = objList.DiscountAmount,
+                MinAmount = objList.MinAmount
+            };
+
+            _response.Result = couponDto;
+
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+
+        return _response;
+    }
+
+    [HttpPost]
+    public ResponseDto Post([FromBody] CouponDto couponDto)
+    {
+        try
+        {
+            Coupon obj = _dbcontext.Coupons.Add(new Coupon
+            {
+                CouponCode = couponDto.CouponCode,
+                DiscountAmount = couponDto.DiscountAmount,
+                MinAmount = couponDto.MinAmount
+            }).Entity;
+
+            _dbcontext.Coupons.Add(obj);
+            _dbcontext.SaveChanges();
+
+            _response.Result = obj;
+
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+
+        return _response;
+    }
+
+    [HttpPut]
+    public ResponseDto Put([FromBody] CouponDto couponDto)
+    {
+        try
+        {
+            Coupon obj = _dbcontext.Coupons.Add(new Coupon
+            {
+                CouponCode = couponDto.CouponCode,
+                DiscountAmount = couponDto.DiscountAmount,
+                MinAmount = couponDto.MinAmount
+            }).Entity;
+
+            _dbcontext.Coupons.Update(obj);
+            _dbcontext.SaveChanges();
+
+            _response.Result = obj;
+
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
+        }
+
+        return _response;
+    }
+
+    [HttpDelete]
+    public ResponseDto Delete(int id)
+    {
+        try
+        {
+            Coupon obj = _dbcontext.Coupons.First(c => c.CouponId == id);
+            _dbcontext.Remove(obj);
+            _dbcontext.SaveChanges();
         }
         catch (Exception ex)
         {
