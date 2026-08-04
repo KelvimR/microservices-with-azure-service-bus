cfdd04d1155f8658e3114eb171caa5adcc9968e6
Kelvim Rodrigues <kelvimrodrigues1@gmail.com>
2026-08-04 15:13:16 -0300

    add migration, dbcontext

--- Mango/Mango.Services.CouponAPI/Models/Coupon.cs
+++ Mango/Mango.Services.CouponAPI/Models/Coupon.cs
@@ -1,16 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-; namespace Mango.Services.CouponAPI.Models
-{    
-    public class Coupon
-    {
-        [Key]
-        public int CouponId { get; set; }
-        [Required]
-        public string CouponCode { get; set; }
-        [Required]
-        public double DiscountAmount { get; set; }
-        public int MinAmount { get; set; }
-    }
+namespace Mango.Services.CouponAPI.Models;
+
+public class Coupon
+{
+    [Key]
+    public int CouponId { get; set; }
+    [Required]
+    public string CouponCode { get; set; }
+    [Required]
+    public double DiscountAmount { get; set; }
+    public int MinAmount { get; set; }
 }
--- Mango/Mango.Services.CouponAPI/Program.cs
+++ Mango/Mango.Services.CouponAPI/Program.cs
@@ -1,6 +1,13 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -22,4 +29,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        
+        if(dbContext.Database.GetPendingMigrations().Count() > 0)
+            dbContext.Database.Migrate();
+        
+    }
+}
